3c28bda5cfda083cd3efc8802d3f943cf931044b
ZephyrTFA <matthew@tfaluc.com>
2023-01-27 01:00:33 -0500

    kek module

---
+++ Hoard2/Module/Builtin/Kek.cs
@@ -0,0 +1,18 @@
+﻿using Discord;
+
+namespace Hoard2.Module.Builtin
+{
+	public class Kek : ModuleBase
+	{
+		public Kek(ulong guildId, string configPath) : base(guildId, configPath) { }
+
+		public override async Task DiscordClientOnMessageReceived(IMessage arg)
+		{
+			if (arg.Source != MessageSource.User)
+				return;
+
+			if (arg.Content.ToLower() == "kek")
+				await arg.Channel.SendMessageAsync("Kek");
+		}
+	}
+}
